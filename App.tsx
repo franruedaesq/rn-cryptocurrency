d5ae1854ca70d126c3fe7c9987dbf36d1d0fa3d3
franruedaesq <franruedaesq@gmail.com>
2021-12-31 18:09:59 -0300

    fix: fonts

--- App.tsx
+++ App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { CryptoDetail, Transaction } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  
   return (
     <NavigationContainer>
       <Stack.Navigator
